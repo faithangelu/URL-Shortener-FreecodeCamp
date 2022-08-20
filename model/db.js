394fb7228eef11d1923882d8a75d69eedefee378
Faith Hidalgo <imfaithangelou@gmail.com>
2022-08-20 15:48:16 -0700

    * updates db

--- model/db.js
+++ model/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { 
+
+mongoose.connect('mongodb://backendfcc:password101@ac-a7mpeer-shard-00-00.ywvccth.mongodb.net:27017,ac-a7mpeer-shard-00-01.ywvccth.mongodb.net:27017,ac-a7mpeer-shard-00-02.ywvccth.mongodb.net:27017/?ssl=true&replicaSet=atlas-ras2pn-shard-0&authSource=admin&retryWrites=true&w=majority', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true }
   ).then(() => console.log("Database connected!"))
